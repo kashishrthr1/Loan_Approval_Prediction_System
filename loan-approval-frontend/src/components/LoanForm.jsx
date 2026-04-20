386baafe790e1abeb3288b4b5aef2a2c9bf1bbfc
kashishrthr1 <kashishrathoreo110@gmail.com>
2026-04-20 15:10:54 +0530

    updation

--- loan-approval-frontend/src/components/LoanForm.jsx
+++ loan-approval-frontend/src/components/LoanForm.jsx
@@ -106,7 +106,7 @@ export default function LoanForm() {
     const payload = buildPayload(form);
     
     // ✅ Wrap in "data" key to match your API
-    const res = await axios.post('http://localhost:5000/predict', {
+    const res = await axios.post('https://loan-approval-prediction-system-1-8yrl.onrender.com', {
       data: payload
     });
 
